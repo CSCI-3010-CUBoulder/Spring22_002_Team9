5bd7ae0c027d0bb7e5ad63f8b711b8b8a9a9076f
Ethan Walters <31104795+ethanfwalters@users.noreply.github.com>
2022-02-08 13:36:41 -0700

    adding the new headers

--- test.cpp
+++ test.cpp
@@ -1,4 +1,6 @@
-#include <catch2/catch.hpp>
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "functions_to_implement.cpp"
 
 TEST_CASE("Signs are correct", "[sign]"){
     REQUIRE(Sign(1) == 1);
